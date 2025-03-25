3a79261f88f49761b1203746a26a089077b83904
Manohar-surnoi <manoharnandelli@surnoi.com>
2025-03-25 18:34:19 +0530

    changes made

---
+++ module-testing/S3/variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  type        = string
+}
+
+variable "project_name" {
+  type        = string
+}
+
+variable "environment" {
+  type        = string
+}
---
+++ modules/s3_bucket/outputs.tf
@@ -0,0 +1,3 @@
+output "bucket_id" {
+  value = aws_s3_bucket.s3.id  # Output the ID of the created S3 bucket
+}
