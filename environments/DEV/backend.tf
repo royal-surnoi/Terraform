c249b2842f5473daa9d6c7afef03b6c4dfc2f1d5
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2025-04-03 15:26:13 +0530

    Multi-env

--- environments/DEV/backend.tf
+++ environments/DEV/backend.tf
@@ -1,5 +1,6 @@
 bucket = "fusioniq-project-dev"
 key    = "global/s3/terraform.tfstate"
 region = "us-east-1"
-dynamodb_table = "fusioniq-tf-lock-dev"
+use_lockfile = true
+# dynamodb_table = "fusioniq-tf-lock-dev"
 
