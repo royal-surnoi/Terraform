a0e9bc04b9a88465b736faefe45bd4283951219d
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2025-04-11 20:51:03 +0530

    terraform

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 output "output" {
   value = <<EOF
-  
+
   ############################# VPC Info ###########################################
   vpc_id                           ${module.vpc.vpc_id}
   public_subnet_ids                ${join(", ", module.vpc.public_subnet_ids)}
@@ -25,48 +25,48 @@ output "output" {
 
 
 
-# VPC
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
+# # VPC
+# output "vpc_id" {
+#   value = module.vpc.vpc_id
+# }
 
-output "public_subnet_ids" {
-  value = module.vpc.public_subnet_ids
-}
+# output "public_subnet_ids" {
+#   value = module.vpc.public_subnet_ids
+# }
 
-output "private_subnet_ids" {
-  value = module.vpc.private_subnet_ids
-}
+# output "private_subnet_ids" {
+#   value = module.vpc.private_subnet_ids
+# }
 
-output "database_subnet_ids" {
-  value = module.vpc.database_subnet_ids
-}
+# output "database_subnet_ids" {
+#   value = module.vpc.database_subnet_ids
+# }
 
-# Bastion
-output "security_group_id" {
-  value = module.security_group.security_group_id
-}
+# # Bastion
+# output "security_group_id" {
+#   value = module.security_group.security_group_id
+# }
 
-output "bastion_public_ip" {
-  value = module.ec2_instance.bastion_public_ip
-}
+# output "bastion_public_ip" {
+#   value = module.ec2_instance.bastion_public_ip
+# }
 
-output "key_name" {
-  value = module.key_pair.key_name
-}
+# output "key_name" {
+#   value = module.key_pair.key_name
+# }
 
-# Amazon RDS
-output "rds_end_point" {
-  value = module.aws-rds.end_point
-}
+# # Amazon RDS
+# output "rds_end_point" {
+#   value = module.aws-rds.end_point
+# }
 
-# AWS EKS
-output "cluster_endpoint" {
-  description = "EKS cluster endpoint"
-  value       = module.eks.cluster_endpoint
-}
+# # AWS EKS
+# output "cluster_endpoint" {
+#   description = "EKS cluster endpoint"
+#   value       = module.eks.cluster_endpoint
+# }
 
-output "cluster_name" {
-  description = "EKS cluster name"
-  value       = module.eks.cluster_name
-}
+# output "cluster_name" {
+#   description = "EKS cluster name"
+#   value       = module.eks.cluster_name
+# }
