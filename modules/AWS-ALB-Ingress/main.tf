793efa69887a79e83965ab08106d6f76cbf6153c
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2025-04-14 12:57:20 +0530

    terraform

--- modules/AWS-ALB-Ingress/main.tf
+++ modules/AWS-ALB-Ingress/main.tf
@@ -1,4 +1,3 @@
-# Kubernetes provider (if not already present)
 provider "kubernetes" {
   host                   = var.cluster_endpoint
   cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
@@ -9,75 +8,6 @@ provider "kubernetes" {
   }
 }
 
-# ClusterRole for AWS Load Balancer Controller
-resource "kubernetes_cluster_role" "alb_ingress_controller" {
-  metadata {
-    name = "alb-ingress-controller"
-  }
-
-  rule {
-    api_groups = [""]
-    resources  = ["pods", "services", "nodes", "endpoints", "namespaces"]
-    verbs      = ["get", "list", "watch"]
-  }
-
-  rule {
-    api_groups = ["extensions", "networking.k8s.io"]
-    resources  = ["ingresses"]
-    verbs      = ["get", "list", "watch", "update", "patch"]
-  }
-
-  rule {
-    api_groups = ["extensions", "networking.k8s.io"]
-    resources  = ["ingresses/status"]
-    verbs      = ["update", "patch"]
-  }
-
-  rule {
-    api_groups = ["coordination.k8s.io"]
-    resources  = ["leases"]
-    verbs      = ["get", "create", "update", "delete", "list", "watch"]
-  }
-
-  rule {
-    api_groups = [""]
-    resources  = ["events"]
-    verbs      = ["create", "patch"]
-  }
-}
-
-# ClusterRoleBinding for AWS Load Balancer Controller
-resource "kubernetes_cluster_role_binding" "alb_ingress_controller" {
-  metadata {
-    name = "alb-ingress-controller"
-  }
-
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = kubernetes_cluster_role.alb_ingress_controller.metadata[0].name
-  }
-
-  subject {
-    kind      = "ServiceAccount"
-    name      = "alb-ingress-controller"
-    namespace = "kube-system"
-  }
-}
-
-# IAM Policy for ALB Ingress Controller
-resource "aws_iam_policy" "alb_ingress" {
-  name   = "${var.cluster_name}-alb-ingress-policy"
-  policy = file("${path.module}/alb-policy.json")
-}
-
-# Attach Policy to Role
-resource "aws_iam_role_policy_attachment" "alb_ingress" {
-  role       = aws_iam_role.alb_ingress.name
-  policy_arn = aws_iam_policy.alb_ingress.arn
-}
-
-# Helm Provider Configuration
 provider "helm" {
   kubernetes {
     host                   = var.cluster_endpoint
@@ -89,64 +19,16 @@ provider "helm" {
     }
   }
 }
-resource "kubernetes_service_account" "aws_load_balancer_controller" {
-  metadata {
-    name      = "aws-load-balancer-controller"
-    namespace = "kube-system"
-  }
-}
 
-# Update Helm release to depend on RBAC resources
-resource "helm_release" "alb_ingress" {
-  name       = "aws-load-balancer-controller"
-  repository = "https://aws.github.io/eks-charts"
-  chart      = "aws-load-balancer-controller"
-  version    = var.alb_ingress_version
-  namespace  = "kube-system"
-  depends_on = [
-    aws_iam_role_policy_attachment.alb_ingress,
-    kubernetes_cluster_role_binding.alb_ingress_controller
-  ]
-
-  set {
-    name  = "clusterName"
-    value = var.cluster_name
-  }
-  set {
-    name  = "serviceAccount.create"
-    value = "true"
-  }
-  set {
-    name  = "serviceAccount.name"
-    value = "alb-ingress-controller"
-  }
-  set {
-    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = aws_iam_role.alb_ingress.arn
-  }
-  # set {
-  #   name  = "image.tag"
-  #   value = var.alb_ingress_image_tag
-  # }
-  set {
-    name  = "vpcId"
-    value = var.vpc_id
-  }
-  set {
-    name  = "rbac.create"
-    value = "true"
-  }
+# IAM Policy for ALB Ingress Controller
+resource "aws_iam_policy" "alb_ingress" {
+  name   = "${var.cluster_name}-alb-ingress-policy"
+  policy = file("${path.module}/alb-policy.json")
 }
 
 # IAM Role for ALB Ingress Controller
-
-
 locals {
   oidc_provider_url = replace(var.oidc_provider_arn, "arn:aws:iam::[0-9]+:oidc-provider/", "")
-}
-
-output "oidc_provider_url" {
-  value = local.oidc_provider_url
 }
 
 resource "aws_iam_role" "alb_ingress" {
@@ -170,41 +52,44 @@ resource "aws_iam_role" "alb_ingress" {
   })
 }
 
+# Attach Policy to Role
+resource "aws_iam_role_policy_attachment" "alb_ingress" {
+  role       = aws_iam_role.alb_ingress.name
+  policy_arn = aws_iam_policy.alb_ingress.arn
+}
 
+# Helm Release for AWS Load Balancer Controller
+resource "helm_release" "alb_ingress" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  version    = var.alb_ingress_version
+  namespace  = "kube-system"
 
+  set {
+    name  = "clusterName"
+    value = var.cluster_name
+  }
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+  set {
+    name  = "serviceAccount.name"
+    value = "alb-ingress-controller"
+  }
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.alb_ingress.arn
+  }
+  set {
+    name  = "vpcId"
+    value = var.vpc_id
+  }
+  set {
+    name  = "rbac.create"
+    value = "true"
+  }
 
-# Helm Release for ALB Ingress Controller
-# resource "helm_release" "alb_ingress" {
-#   name       = "aws-load-balancer-controller"
-#   repository = "https://aws.github.io/eks-charts"
-#   chart      = "aws-load-balancer-controller"
-#   version    = var.alb_ingress_version
-#   namespace  = "kube-system"
-#   depends_on = [aws_iam_role_policy_attachment.alb_ingress]
-
-#   set {
-#     name  = "clusterName"
-#     value = var.cluster_name
-#   }
-#   set {
-#     name  = "serviceAccount.create"
-#     value = "true"
-#   }
-#   set {
-#     name  = "serviceAccount.name"
-#     value = "alb-ingress-controller"
-#   }
-#   set {
-#     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-#     value = aws_iam_role.alb_ingress.arn
-#   }
-#   # set {
-#   #   name  = "image.tag"
-#   #   value = var.alb_ingress_image_tag
-#   # }
-#   set {
-#     name  = "vpcId"
-#     value = var.vpc_id
-#   }
-# }
-
+  depends_on = [aws_iam_role_policy_attachment.alb_ingress]
+}
--- modules/AWS-ALB-Ingress/output.tf
+++ modules/AWS-ALB-Ingress/output.tf
@@ -1,8 +1,3 @@
-# output "oidc_provider_arn" {
-#   description = "ARN of the OIDC provider for the EKS cluster"
-#   value       = data.oidc_provider_arn.arn
-# }
-
 output "alb_ingress_role_arn" {
   description = "ARN of the IAM role for ALB Ingress Controller"
   value       = aws_iam_role.alb_ingress.arn
