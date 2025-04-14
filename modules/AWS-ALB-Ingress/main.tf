be7adac13eee1eeb489fad21a53b11c0dd50ea9a
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2025-04-14 10:37:41 +0530

    terraform

--- modules/AWS-ALB-Ingress/main.tf
+++ modules/AWS-ALB-Ingress/main.tf
@@ -65,47 +65,72 @@ resource "kubernetes_cluster_role_binding" "alb_ingress_controller" {
   }
 }
 
-# Update Helm release to depend on RBAC resources
-# resource "helm_release" "alb_ingress" {
-#   name       = "aws-load-balancer-controller"
-#   repository = "https://aws.github.io/eks-charts"
-#   chart      = "aws-load-balancer-controller"
-#   version    = var.alb_ingress_version
-#   namespace  = "kube-system"
-#   depends_on = [
-#     aws_iam_role_policy_attachment.alb_ingress,
-#     kubernetes_cluster_role_binding.alb_ingress_controller
-#   ]
+# IAM Policy for ALB Ingress Controller
+resource "aws_iam_policy" "alb_ingress" {
+  name   = "${var.cluster_name}-alb-ingress-policy"
+  policy = file("${path.module}/alb-policy.json")
+}
 
-#   set {
-#     name  = "clusterName"
-#     value = var.cluster_name
-#   }
-#   set {
-#     name  = "serviceAccount.create"
-#     value = "true"
-#   }
-#   set {
-#     name  = "serviceAccount.name"
-#     value = "alb-ingress-controller"
-#   }
-#   set {
-#     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-#     value = aws_iam_role.alb_ingress.arn
-#   }
-#   set {
-#     name  = "image.tag"
-#     value = var.alb_ingress_image_tag
-#   }
-#   set {
-#     name  = "vpcId"
-#     value = var.vpc_id
-#   }
-#   set {
-#     name  = "rbac.create"
-#     value = "true"
-#   }
-# }
+# Attach Policy to Role
+resource "aws_iam_role_policy_attachment" "alb_ingress" {
+  role       = aws_iam_role.alb_ingress.name
+  policy_arn = aws_iam_policy.alb_ingress.arn
+}
+
+# Helm Provider Configuration
+provider "helm" {
+  kubernetes {
+    host                   = var.cluster_endpoint
+    cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    }
+  }
+}
+
+# Update Helm release to depend on RBAC resources
+resource "helm_release" "alb_ingress" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  version    = var.alb_ingress_version
+  namespace  = "kube-system"
+  depends_on = [
+    aws_iam_role_policy_attachment.alb_ingress,
+    kubernetes_cluster_role_binding.alb_ingress_controller
+  ]
+
+  set {
+    name  = "clusterName"
+    value = var.cluster_name
+  }
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+  set {
+    name  = "serviceAccount.name"
+    value = "alb-ingress-controller"
+  }
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.alb_ingress.arn
+  }
+  # set {
+  #   name  = "image.tag"
+  #   value = var.alb_ingress_image_tag
+  # }
+  set {
+    name  = "vpcId"
+    value = var.vpc_id
+  }
+  set {
+    name  = "rbac.create"
+    value = "true"
+  }
+}
 
 # IAM Role for ALB Ingress Controller
 
@@ -140,63 +165,40 @@ resource "aws_iam_role" "alb_ingress" {
 }
 
 
-# IAM Policy for ALB Ingress Controller
-resource "aws_iam_policy" "alb_ingress" {
-  name   = "${var.cluster_name}-alb-ingress-policy"
-  policy = file("${path.module}/alb-policy.json")
-}
 
-# Attach Policy to Role
-resource "aws_iam_role_policy_attachment" "alb_ingress" {
-  role       = aws_iam_role.alb_ingress.name
-  policy_arn = aws_iam_policy.alb_ingress.arn
-}
-
-# Helm Provider Configuration
-provider "helm" {
-  kubernetes {
-    host                   = var.cluster_endpoint
-    cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
-    }
-  }
-}
 
 # Helm Release for ALB Ingress Controller
-resource "helm_release" "alb_ingress" {
-  name       = "aws-load-balancer-controller"
-  repository = "https://aws.github.io/eks-charts"
-  chart      = "aws-load-balancer-controller"
-  version    = var.alb_ingress_version
-  namespace  = "kube-system"
-  depends_on = [aws_iam_role_policy_attachment.alb_ingress]
+# resource "helm_release" "alb_ingress" {
+#   name       = "aws-load-balancer-controller"
+#   repository = "https://aws.github.io/eks-charts"
+#   chart      = "aws-load-balancer-controller"
+#   version    = var.alb_ingress_version
+#   namespace  = "kube-system"
+#   depends_on = [aws_iam_role_policy_attachment.alb_ingress]
 
-  set {
-    name  = "clusterName"
-    value = var.cluster_name
-  }
-  set {
-    name  = "serviceAccount.create"
-    value = "true"
-  }
-  set {
-    name  = "serviceAccount.name"
-    value = "alb-ingress-controller"
-  }
-  set {
-    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = aws_iam_role.alb_ingress.arn
-  }
-  # set {
-  #   name  = "image.tag"
-  #   value = var.alb_ingress_image_tag
-  # }
-  set {
-    name  = "vpcId"
-    value = var.vpc_id
-  }
-}
+#   set {
+#     name  = "clusterName"
+#     value = var.cluster_name
+#   }
+#   set {
+#     name  = "serviceAccount.create"
+#     value = "true"
+#   }
+#   set {
+#     name  = "serviceAccount.name"
+#     value = "alb-ingress-controller"
+#   }
+#   set {
+#     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+#     value = aws_iam_role.alb_ingress.arn
+#   }
+#   # set {
+#   #   name  = "image.tag"
+#   #   value = var.alb_ingress_image_tag
+#   # }
+#   set {
+#     name  = "vpcId"
+#     value = var.vpc_id
+#   }
+# }
 
